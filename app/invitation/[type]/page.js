490d2007a089d96b14541dcf4f00dc3b1af0ff61
ThuongPhan662003 <thuong5so55555@gmail.com>
2026-06-07 03:53:40 +0700

    ư

--- app/invitation/[type]/page.js
+++ app/invitation/[type]/page.js
@@ -2,7 +2,24 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 
 // import GuestConfirmForm from "@/components/Invitation/GuestConfirmForm";
+import { notFound } from "next/navigation";
 
+export function generateMetadata({ params }) {
+  const weddingConfig = {
+    nha_trai: {
+      ceremonyName: "LỄ THÀNH HÔN",
+    },
+    nha_gai: {
+      ceremonyName: "LỄ VU QUY",
+    },
+  };
+
+  const current = weddingConfig[params.type];
+
+  return {
+    title: current?.ceremonyName || "Thiệp cưới ❤️",
+  };
+}
 export default async function InvitationPage({ params }) {
   const resolvedParams = await params;
 
