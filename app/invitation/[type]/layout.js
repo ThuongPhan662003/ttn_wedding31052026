1f00a2b05761212761a1642d1c31c67e3a5d2419
Tô Phan Kiều Thương <125787847+ThuongPhan662003@users.noreply.github.com>
2026-06-07 18:52:41 +0700

    Update layout.js

--- app/invitation/[type]/layout.js
+++ app/invitation/[type]/layout.js
@@ -1,11 +1,40 @@
 export async function generateMetadata({ params }) {
   const { type } = await params;
 
+  const isNhaTrai = type === "nha_trai";
+
   return {
-    title:
-      type === "nha_trai"
-        ? "LỄ THÀNH HÔN"
-        : "LỄ VU QUY",
+    title: isNhaTrai
+      ? "Lễ Tân Hôn: Trọng Nghĩa & Thu Thảo"
+      : "Lễ Vu Quy: Thu Thảo & Trọng Nghĩa",
+
+    description:
+      "Trân trọng kính mời bạn tham dự ngày vui của chúng tôi.",
+
+    openGraph: {
+      title: isNhaTrai
+        ? "Lễ Tân Hôn: Trọng Nghĩa & Thu Thảo"
+        : "Lễ Vu Quy: Thu Thảo & Trọng Nghĩa",
+
+      description:
+        "Trân trọng kính mời bạn tham dự ngày vui của chúng tôi.",
+
+      images: [
+        {
+          url: "https://thaonghiawedding.vercel.app/images/open-graph.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Thiệp cưới Trọng Nghĩa & Thu Thảo",
+        },
+      ],
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      images: [
+        "https://thaonghiawedding.vercel.app/images/open-graph.jpg",
+      ],
+    },
   };
 }
 
