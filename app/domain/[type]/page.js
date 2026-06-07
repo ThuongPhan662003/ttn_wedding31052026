ae429504aff0432a126ef733a1e1e628625e1097
Tô Phan Kiều Thương <125787847+ThuongPhan662003@users.noreply.github.com>
2026-06-07 11:43:33 +0700

    Create domain/[type]/page.js

---
+++ app/domain/[type]/page.js
@@ -0,0 +1,24 @@
+type Props = {
+  params: Promise<{
+    type: string;
+  }>;
+};
+
+export async function generateMetadata({ params }: Props) {
+  const { type } = await params;
+
+  const titleMap: Record<string, string> = {
+    nha_trai: "Lễ Tân Hôn: Trọng Nghĩa & Thu Thảo",
+    nha_gai: "Lễ Vu Quy: Thu Thảo & Trọng Nghĩa",
+  };
+
+  return {
+    title: titleMap[type] || "Trang mặc định",
+  };
+}
+
+export default async function Page({ params }: Props) {
+  const { type } = await params;
+
+  return <div>{type}</div>;
+}
